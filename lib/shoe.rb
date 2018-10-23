5ea9cdc8961804aa4daa3f3ae0c289baa7512159
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-23 21:08:54 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  brands = []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -15,7 +15,7 @@ class Shoe
 
   def BRANDS(brand)
     @brand = brand
-    brands << brand
+    BRANDS << brand
   end
 
 end
